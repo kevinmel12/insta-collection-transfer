4851d3062e5ab12459618128bc35b34cbe088996
unknown <melkevinmeledje@gmail.com>
2024-09-14 22:22:30 +0200

    Login and redirect to the collection in the second account .

--- script.py
+++ script.py
@@ -12,6 +12,10 @@ options = Options()
 options.add_experimental_option("detach", True)
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
+
+ # ------------------------------- PREMIER COMPTE INSTA -------------------------------
+
+
 # Créer une instance de WebDriver
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
@@ -20,6 +24,7 @@ try:
     url = "https://www.instagram.com/"
     browser.get(url)
 
+
     # Attendre et interagir avec l'élément après l'ouverture de la page
     time.sleep(2)
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
@@ -27,6 +32,7 @@ try:
     )).click()
 
     time.sleep(5)
+
 
     # Lire le mot de passe et le pseudo à partir du fichier `.env`
     with open('.env', 'r') as file:
@@ -53,8 +59,57 @@ try:
     WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
 
-    time.sleep(9000) 
+    # ------------------------------- SECOND COMPTE INSTA -------------------------------
 
 
+    browser2 = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    try:
+        # Ouvrir la page web Instagram dans la nouvelle fenêtre
+        url = "https://www.instagram.com/"
+        browser2.get(url)
+
+
+        # Attendre et interagir avec l'élément après l'ouverture de la page
+        time.sleep(2)
+        WebDriverWait(browser2, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]')
+        )).click()
+
+        time.sleep(5)
+
+            # Lire le mot de passe et le pseudo à partir du fichier `.env`
+        with open('.env', 'r') as file:
+            lines = file.readlines()
+            username = lines[2].strip().split('=')[1]
+            password = lines[3].strip().split('=')[1]
+
+        # Remplir les champs d'authentification
+        WebDriverWait(browser2, 10).until(EC.presence_of_element_located((By.NAME, 'username'))).send_keys(username)
+        browser2.find_element(By.NAME, 'password').send_keys(password)
+
+        # Cliquer sur le bouton de connexion
+        WebDriverWait(browser2, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div/div[3]/button')
+        )).click()
+
+        time.sleep(10)  
+
+        # Naviguer vers la page de collection
+        saves_url = f"https://www.instagram.com/{username}/saved/_/17927259749817910/"
+        browser2.get(saves_url)
+
+
+        # Attendre que la page se charge
+        WebDriverWait(browser2, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            
+
+        time.sleep(9000)
+    finally:
+        browser2.quit()
+
+
+    time.sleep(9000)
 finally:
     browser.quit()
+
+ 
